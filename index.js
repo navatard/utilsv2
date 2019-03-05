fed0b4e3d4adf059fb5e211c7438f0f72a93aa1d
mgarnier11 <mgarnier11@gmail.com>
2019-03-05 16:34:23 +0100

    better tests

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
+const errors = {
+    notANumber: 'nb is not a number',
+    nbTooHigh: 'nb is too high',
+    nbTooLow: 'nb is too low'
+}
+
 function add(nb1, nb2) {
-    validate(nb1, nb2);
+    validateNumber(nb1);
+    validateNumber(nb2);
     return nb1 + nb2;
 }
 
 function multiply(nb1, nb2) {
-    validate(nb1, nb2);
+    validateNumber(nb1);
+    validateNumber(nb2);
     return nb1 * nb2;
 }
 
 function substract(nb1, nb2) {
-    validate(nb1, nb2);
+    validateNumber(nb1);
+    validateNumber(nb2);
     return nb1 - nb2;
 }
 
 function divide(nb1, nb2) {
-    validate(nb1, nb2);
+    validateNumber(nb1);
+    validateNumber(nb2);
     return nb1 / nb2;
 }
 
-function validate(nb1, nb2) {
-    if (typeof nb1 != 'number') throw new Error('number1 is not a number');
-    if (typeof nb2 != 'number') throw new Error('number2 is not a number');
+function validateNumber(nb) {
+    if (typeof nb != 'number') throw new Error(errors.notANumber);
+
+    if (nb > Number.MAX_SAFE_INTEGER) throw new Error(errors.nbTooHigh);
+    else if (nb < -Number.MAX_SAFE_INTEGER) throw new Error(errors.nbTooLow);
 }
 
 module.exports = {
     add: add,
     multiply: multiply,
     substract: substract,
-    divide: divide
+    divide: divide,
+    errors: errors
 }
--- test.js
+++ test.js
@@ -1,28 +1,51 @@
 const assert = require('assert');
 const operations = require('./index');
 
-it('add(1,1) should return 2', () => {
-    assert.equal(operations.add(1, 1), 2);
-});
-it('multiply(2,2) should return 4', () => {
-    assert.equal(operations.multiply(2, 2), 4);
-});
-it('substract(4,1) should return 3', () => {
-    assert.equal(operations.substract(4, 1), 3);
-});
-it('divide(4,2) should return 2', () => {
-    assert.equal(operations.divide(4, 2), 2);
-});
+describe('operation package testing', () => {
 
-it('add("ceci n\'est pas un nombre", 3) should return an error', () => {
-    assert.throws(() => operations.add("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
-});
-it('multiply("ceci n\'est pas un nombre", 3) should return an error', () => {
-    assert.throws(() => operations.multiply("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
-});
-it('substract("ceci n\'est pas un nombre", 3) should return an error', () => {
-    assert.throws(() => operations.substract("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
-});
-it('divide("ceci n\'est pas un nombre", 3) should return an error', () => {
-    assert.throws(() => operations.divide("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
-});
+    describe('check base operations', () => {
+        it('add(1,1) should return 2', () => {
+            assert.equal(operations.add(1, 1), 2);
+        });
+        it('multiply(2,2) should return 4', () => {
+            assert.equal(operations.multiply(2, 2), 4);
+        });
+        it('substract(4,1) should return 3', () => {
+            assert.equal(operations.substract(4, 1), 3);
+        });
+        it('divide(4,2) should return 2', () => {
+            assert.equal(operations.divide(4, 2), 2);
+        });
+    });
+
+    describe('check if input is a number', () => {
+        it('add("ceci n\'est pas un nombre", 3) should return an error', () => {
+            assert.throws(() => operations.add("ceci n'est pas un nombre", 3), Error, operations.errors.notANumber);
+        });
+        it('multiply("ceci n\'est pas un nombre", 3) should return an error', () => {
+            assert.throws(() => operations.multiply("ceci n'est pas un nombre", 3), Error, operations.errors.notANumber);
+        });
+        it('substract("ceci n\'est pas un nombre", 3) should return an error', () => {
+            assert.throws(() => operations.substract("ceci n'est pas un nombre", 3), Error, operations.errors.notANumber);
+        });
+        it('divide("ceci n\'est pas un nombre", 3) should return an error', () => {
+            assert.throws(() => operations.divide("ceci n'est pas un nombre", 3), Error, operations.errors.notANumber);
+        });
+    })
+
+    describe('check if number is in range', () => {
+
+        it('add(9007199254740992, 3) should return an error', () => {
+            assert.throws(() => operations.add(9007199254740992, 3), Error, operations.errors.nbTooHigh);
+        });
+        it('multiply(9007199254740992, 3) should return an error', () => {
+            assert.throws(() => operations.multiply(9007199254740992, 3), Error, operations.errors.nbTooHigh);
+        });
+        it('substract(9007199254740992, 3) should return an error', () => {
+            assert.throws(() => operations.substract(9007199254740992, 3), Error, operations.errors.nbTooHigh);
+        });
+        it('divide(9007199254740992, 3) should return an error', () => {
+            assert.throws(() => operations.divide(9007199254740992, 3), Error, operations.errors.nbTooHigh);
+        });
+    });
+})
