94f4d090afe3b4482f2b87e21e70965a0cc89f20
mgarnier11 <mgarnier11@gmail.com>
2019-03-05 15:37:45 +0100

    .

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 function add(nb1, nb2) {
+    validate(nb1, nb2);
     return nb1 + nb2;
 }
 
 function multiply(nb1, nb2) {
+    validate(nb1, nb2);
     return nb1 * nb2;
 }
 
 function substract(nb1, nb2) {
+    validate(nb1, nb2);
     return nb1 - nb2;
 }
 
 function divide(nb1, nb2) {
+    validate(nb1, nb2);
     return nb1 / nb2;
+}
+
+function validate(nb1, nb2) {
+    if (typeof nb1 != 'number') throw new Error('number1 is not a number');
+    if (typeof nb2 != 'number') throw new Error('number2 is not a number');
 }
 
 module.exports = {
--- test.js
+++ test.js
@@ -1,30 +1,28 @@
 const assert = require('assert');
 const operations = require('./index');
 
-let base = 1;
-
-console.log('base is : ' + base);
-
-it('base should be 2', () => {
-    base = operations.add(base, 1);
-
-    assert.equal(base, 2);
+it('add(1,1) should return 2', () => {
+    assert.equal(operations.add(1, 1), 2);
+});
+it('multiply(2,2) should return 4', () => {
+    assert.equal(operations.multiply(2, 2), 4);
+});
+it('substract(4,1) should return 3', () => {
+    assert.equal(operations.substract(4, 1), 3);
+});
+it('divide(4,2) should return 2', () => {
+    assert.equal(operations.divide(4, 2), 2);
 });
 
-it('base should be 4', () => {
-    base = operations.multiply(base, 2);
-
-    assert.equal(base, 4);
+it('add("ceci n\'est pas un nombre", 3) should return an error', () => {
+    assert.throws(() => operations.add("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
 });
-
-it('base should be 3', () => {
-    base = operations.substract(base, 1);
-
-    assert.equal(base, 3);
+it('multiply("ceci n\'est pas un nombre", 3) should return an error', () => {
+    assert.throws(() => operations.multiply("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
 });
-
-it('base should be 1', () => {
-    base = operations.divide(base, 3);
-
-    assert.equal(base, 1);
+it('substract("ceci n\'est pas un nombre", 3) should return an error', () => {
+    assert.throws(() => operations.substract("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
+});
+it('divide("ceci n\'est pas un nombre", 3) should return an error', () => {
+    assert.throws(() => operations.divide("ceci n'est pas un nombre", 3), Error, 'number1 is not a number');
 });
